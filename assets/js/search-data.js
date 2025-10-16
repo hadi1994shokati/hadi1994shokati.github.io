e1f636756f6add0a8732383018699395ca43f0c2
hadi1994shokati <148059512+hadi1994shokati@users.noreply.github.com>
2025-10-16 15:57:07 +0000

    Deploying to gh-pages from @ hadi1994shokati/hadi1994shokati.github.io@dc4ec6f3f5de6530a1bc1ac915817f40e998e06a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,9 +446,9 @@ ninja.data = [{
           description: "Using fine-tuned SAM and ResNet-backboned U-Net",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-soil-moisture-estimation",
+          title: 'Soil Moisture Estimation',
+          description: "Comparing UAV hyperspectral and satellite multispectral approaches",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
