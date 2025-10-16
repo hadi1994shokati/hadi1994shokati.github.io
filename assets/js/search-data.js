6d38e70df18fbafdd1953942d5d8a09fb51562e1
hadi1994shokati <148059512+hadi1994shokati@users.noreply.github.com>
2025-10-16 13:10:32 +0000

    Deploying to gh-pages from @ hadi1994shokati/hadi1994shokati.github.io@02988be2f726196c94fd8b72c40d078e21034211 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,9 +431,9 @@ ninja.data = [{
           description: "Semantic segmentation of soil erosion by water",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-hybrid-soil-erosion-modeling",
+          title: 'Hybrid Soil Erosion Modeling',
+          description: "Combining physics-based and deep learning approaches",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
