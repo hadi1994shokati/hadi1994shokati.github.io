03ffebd6d8c28b8ff1a5f19a40b99c7450de3097
hadi1994shokati <148059512+hadi1994shokati@users.noreply.github.com>
2025-10-16 15:18:19 +0000

    Deploying to gh-pages from @ hadi1994shokati/hadi1994shokati.github.io@0694217072d9085f06cbfe496c5593c258dcfe91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,9 +441,9 @@ ninja.data = [{
           description: "Historical reconstruction and future prediction using ConvLSTM",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-rapid-flood-mapping",
+          title: 'Rapid Flood Mapping',
+          description: "Using fine-tuned SAM and ResNet-backboned U-Net",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
