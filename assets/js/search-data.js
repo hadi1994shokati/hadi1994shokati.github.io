c940604e214fe60fd42e5dc2e973b91a85eba407
hadi1994shokati <148059512+hadi1994shokati@users.noreply.github.com>
2025-10-17 09:52:49 +0000

    Deploying to gh-pages from @ hadi1994shokati/hadi1994shokati.github.io@aebdb230c1f0c422b56143f37a19d407a7ce87b2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-teaching",
-    title: "teaching",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -39,10 +39,10 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "",
+          description: "Teaching Assistant experience at the University of Tehran.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/";
+            window.location.href = "/Teaching%20Assistant/";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
